d9798b808139f264e46610ed1b96a2e015ead7c6
Ilya Olovyannikov <excaliburgyro@gmail.com>
2023-11-04 13:45:11 +0300

    Add client dev init

---
+++ packages/client/src/app/App.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react'
+
+export const App = () => {
+    useEffect(() => {
+        const fetchServerData = async () => {
+            const url = `http://localhost:${__SERVER_PORT__}`
+            const response = await fetch(url)
+            const data = await response.json()
+            console.log(data)
+        }
+
+        fetchServerData()
+    }, [])
+    return <div className="App">Вот тут будет жить ваше приложение :)</div>
+}
