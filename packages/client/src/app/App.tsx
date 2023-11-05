fad1206eb18a0fc854fb710cf1df5700191e111b
Ilya Olovyannikov <excaliburgyro@gmail.com>
2023-11-05 14:04:49 +0300

    Add server boilerplate

--- packages/client/src/app/App.tsx
+++ packages/client/src/app/App.tsx
@@ -1,15 +1,15 @@
-import { useEffect } from 'react'
+import { useEffect } from 'react';
 
 export const App = () => {
     useEffect(() => {
         const fetchServerData = async () => {
-            const url = `http://localhost:${__SERVER_PORT__}`
-            const response = await fetch(url)
-            const data = await response.json()
-            console.log(data)
-        }
+            const url = `http://localhost:${__SERVER_PORT__}`;
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+        };
 
-        fetchServerData()
-    }, [])
-    return <div className="App">Вот тут будет жить ваше приложение :)</div>
-}
+        fetchServerData();
+    }, []);
+    return <div className="App">Вот тут будет жить ваше приложение :)</div>;
+};
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
 export default defineConfig({
     server: {
         port: Number(process.env.CLIENT_PORT) || 3000,
     },
     define: {
-        __SERVER_PORT__: process.env.SERVER_PORT,
+        __SERVER_PORT__: Number(process.env.SERVER_PORT) || 3001,
     },
     plugins: [react()],
 });
