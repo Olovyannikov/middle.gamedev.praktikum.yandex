4a3d85ca9a483250528ec8078b1178d577dfb1d1
Ilya Olovyannikov <excaliburgyro@gmail.com>
2023-11-05 20:04:28 +0300

    feat(infra): add initial project settings (GAM-10)

---
+++ packages/client/src/pages/IndexPage/index.tsx
@@ -0,0 +1,5 @@
+import s from './IndexPage.module.scss';
+
+export const IndexPage = () => {
+    return <h1 className={s.test}>Hello World!</h1>;
+};
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,15 +1,43 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-export default defineConfig({
-    server: {
-        port: Number(process.env.CLIENT_PORT) || 3000,
-    },
-    define: {
-        __SERVER_PORT__: Number(process.env.SERVER_PORT) || 3001,
-    },
-    plugins: [react()],
+const generateScopedName = '[name]__[local]--[hash:base64:5]';
+const hashName = '[hash:base64:5]';
+
+export default defineConfig(({ mode }) => {
+    const cssModulesName =
+        mode === 'development'
+            ? generateScopedName.replace('-module', '')
+            : hashName;
+
+    return {
+        css: {
+            modules: {
+                generateScopedName: cssModulesName,
+            },
+            preprocessorOptions: {
+                scss: {
+                    additionalData:
+                        '@import "./src/app/assets/styles/general/_mixins.scss";',
+                },
+            },
+            devSourcemap: true,
+        },
+        server: {
+            port: Number(process.env.CLIENT_PORT) || 3000,
+        },
+        define: {
+            __SERVER_PORT__: Number(process.env.SERVER_PORT) || 3001,
+        },
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            },
+        },
+        plugins: [react()],
+    };
 });
