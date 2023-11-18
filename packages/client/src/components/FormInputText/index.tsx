a427cbf11e6f32eba20bdbc5c130aa70869027ab
tinymelon <e.pratusevich@gmail.com>
2023-11-18 15:44:22 +0300

    feat(login-form): add login form (GAM-15) (#5)
    * feat: add static login form (GAM-15)
    
    * feat: login form styles improvements (GAM-15)
    
    * feat: login form components improvements (GAM-15)
    
    * fix: control from context, form semantics (GAM-15)
    
    * fix: unused Control definition removed (GAM-15)

---
+++ packages/client/src/components/FormInputText/index.tsx
@@ -0,0 +1,30 @@
+import { Controller, useFormContext } from 'react-hook-form';
+import TextField from '@mui/material/TextField';
+
+interface FormInputTextProps {
+    name: string;
+    label: string;
+    setValue?: unknown;
+}
+
+export const FormInputText = ({ name, label }: FormInputTextProps) => {
+    const { control } = useFormContext();
+
+    return (
+        <Controller
+            name={name}
+            control={control}
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+                <TextField
+                    helperText={error ? error.message : null}
+                    error={!!error}
+                    onChange={onChange}
+                    variant={'standard'}
+                    value={value}
+                    label={label}
+                    fullWidth
+                />
+            )}
+        />
+    );
+};
