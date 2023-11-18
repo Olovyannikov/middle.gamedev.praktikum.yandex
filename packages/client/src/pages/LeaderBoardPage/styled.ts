6f65c1447c1badbbcf63a5315fe34b3a9b2626ad
Маки Макиев <useful22052@gmail.com>
2023-11-18 16:32:59 +0300

    feat/gam-20

---
+++ packages/client/src/pages/LeaderBoardPage/styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const TextBadgeUI = styled.div`
+    padding: 5px 10px;
+    border-radius: 15%;
+
+    background-color: rgba(30, 54, 141, 0.2);
+    color: black;
+
+    font-size: 12px;
+    font-weight: 600;
+`;
