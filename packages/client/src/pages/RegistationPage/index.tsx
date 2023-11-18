d1aab6f23e9670b5fff84f741b640e75c412b5ef
tinymelon <e.pratusevich@gmail.com>
2023-11-18 17:17:20 +0300

    feat(registration-form): add registration layout (GAM-18)

--- packages/client/src/pages/RegistationPage/index.tsx
+++ packages/client/src/pages/RegistationPage/index.tsx
@@ -1,9 +1,69 @@
 import { RootLayout } from '@/layouts/RootLayout';
+import { Container, Button, Typography } from '@mui/material';
+import { useForm, SubmitHandler, FormProvider } from 'react-hook-form';
+import { FormInputText } from '@/components/FormInputText';
+import { FormPaperWrapper } from '@/components/FormPaperWrapper';
+import s from './RegistrationPage.module.scss';
+
+interface RegistrationParams {
+    login: string;
+    password: string;
+    first_name: string;
+    second_name: string;
+    phone: string;
+    email: string;
+}
 
 export default function RegistrationPage() {
+    const methods = useForm<RegistrationParams>({
+        defaultValues: {
+            login: '',
+            password: '',
+            first_name: '',
+            second_name: '',
+            phone: '',
+            email: '',
+        },
+    });
+
+    const onSubmit: SubmitHandler<RegistrationParams> = (data) => {
+        console.log(data);
+    };
+
     return (
         <RootLayout>
-            <h1>Hello Registration!</h1>
+            <Container>
+                <FormPaperWrapper>
+                    <Typography variant="h4">Зарегистрироваться</Typography>
+                    <FormProvider {...methods}>
+                        <form
+                            onSubmit={methods.handleSubmit(onSubmit)}
+                            className={s.form}
+                        >
+                            <FormInputText label="Логин" name={'login'} />
+                            <FormInputText label="Имя" name={'first_name'} />
+                            <FormInputText
+                                label="Фамилия"
+                                name={'second_name'}
+                            />
+                            <FormInputText label="Телефон" name={'phone'} />
+                            <FormInputText label="E-mail" name={'email'} />
+                            <FormInputText label="Пароль" name={'password'} />
+                            <FormInputText
+                                label="Повторите пароль"
+                                name={'repeatPassword'}
+                            />
+                            <Button
+                                type={'submit'}
+                                variant={'contained'}
+                                className={s.formButton}
+                            >
+                                Зарегистрироваться
+                            </Button>
+                        </form>
+                    </FormProvider>
+                </FormPaperWrapper>
+            </Container>
         </RootLayout>
     );
 }
