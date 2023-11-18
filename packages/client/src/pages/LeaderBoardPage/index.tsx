10d65978afd3bf41b1f2d9d396732db40757ba2e
Маки Макиев <useful22052@gmail.com>
2023-11-18 16:03:45 +0300

    feat/gam-14

--- packages/client/src/pages/LeaderBoardPage/index.tsx
+++ packages/client/src/pages/LeaderBoardPage/index.tsx
@@ -1,9 +1,18 @@
 import { RootLayout } from '@/layouts/RootLayout';
+import { Grid } from '@mui/material';
 
 export default function LeaderBoardPage() {
     return (
         <RootLayout>
-            <h1>Hello Leaderboard!</h1>
+            <Grid
+                container
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                style={{ minHeight: '90vh' }}
+            >
+                <h1>Hello Leaderboard!</h1>
+            </Grid>
         </RootLayout>
     );
 }
