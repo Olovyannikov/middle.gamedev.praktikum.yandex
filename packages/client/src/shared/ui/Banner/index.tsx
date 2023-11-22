57e508e70b517dd89634b7b12de83e60eb260ec9
Evgeniy Talagaev <80750542+WeHaveBOOST@users.noreply.github.com>
2023-11-22 19:24:27 +0300

    feat(main page): add main page (GAM-12) (#13)
    * init(client): add Banner, change RootLayout GAM-12
    
    * feat(client): add fonts and img folder, finish GamePage
    
    * refactor(client): add fonts custom-properties, move const variables in config.ts, ... (GAM-12)
    
    ---------
    
    Co-authored-by: ee.talagaev <ee.talagaev@tensor.ru>

---
+++ packages/client/src/shared/ui/Banner/index.tsx
@@ -0,0 +1,28 @@
+import { Container } from '../Container';
+import { Typography } from '@mui/material';
+
+import s from './Banner.module.scss';
+
+interface BannerProps {
+    title?: string;
+    description?: string;
+}
+
+export const Banner = ({ title, description }: BannerProps) => {
+    return (
+        <section className={s.banner}>
+            <Container className={s.content}>
+                {title ? (
+                    <Typography variant="h1" className={s.title}>
+                        {title}
+                    </Typography>
+                ) : null}
+                {description ? (
+                    <Typography className={s.description}>
+                        {description}
+                    </Typography>
+                ) : null}
+            </Container>
+        </section>
+    );
+};
