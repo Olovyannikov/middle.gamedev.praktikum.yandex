97531709d5394c1d1f2e1bc6b10acfcc850f7c62
Ilya Olovyannikov <excaliburgyro@gmail.com>
2023-12-02 19:34:42 +0300

    fix(conflicts): resolve merge conflicts (GAM-23)

--- packages/client/src/widgets/Game/constants/index.ts
+++ packages/client/src/widgets/Game/constants/index.ts
@@ -1,3 +1,5 @@
+import { getRandomInt } from '@/shared/lib/getRandomInt';
+
 export const GAME_STATE = {
     RUNNING: 'RUNNING',
     PAUSED: 'PAUSED',
@@ -12,4 +14,27 @@ export const DIRECTIONS = {
     RIGHT: 'RIGHT',
 } as const;
 
-export const SEGMENT_SIZE = 5;
+export const GAME_CONTROLS = {
+    UP: 'ArrowUp',
+    DOWN: 'ArrowDown',
+    LEFT: 'ArrowLeft',
+    RIGHT: 'ArrowRight',
+    KEY_W: 'KeyW',
+    KEY_S: 'KeyS',
+    KEY_A: 'KeyA',
+    KEY_D: 'KeyD',
+    SPACE: 'Space',
+};
+
+const DIRECTIONS_KEYS = Object.keys(DIRECTIONS);
+
+export const DIRECTIONS_COUNT = DIRECTIONS_KEYS.length - 1;
+
+export const SETUP_POSITION =
+    DIRECTIONS[
+        DIRECTIONS_KEYS[
+            getRandomInt(0, DIRECTIONS_COUNT)
+        ] as keyof typeof DIRECTIONS
+    ];
+
+export const SEGMENT_SIZE = 6;
