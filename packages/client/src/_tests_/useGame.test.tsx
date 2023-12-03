466813fd6b75897876bd06c8894c31462cc49256
Маки Макиев <useful22052@gmail.com>
2023-12-03 16:17:23 +0300

    feat/GAM-34

--- packages/client/src/_tests_/useGame.test.tsx
+++ packages/client/src/_tests_/useGame.test.tsx
@@ -2,6 +2,7 @@ import { renderHook } from '@testing-library/react';
 import { useGame } from '../widgets/Game/useGame';
 import { GAME_STATE } from '../widgets/Game/constants';
 import { FC, createContext } from 'react';
+import { act } from 'react-dom/test-utils';
 
 describe('useGame Hook', () => {
     const mockOnGameOver = jest.fn();
@@ -14,44 +15,48 @@ describe('useGame Hook', () => {
         <WrappperContext.Provider value="">{children}</WrappperContext.Provider>
     );
 
-    it('initializes game state correctly', () => {
-        const { result } = renderHook(
-            () =>
-                useGame({
-                    gameState: GAME_STATE.RUNNING,
-                    onGameOver: mockOnGameOver,
-                    setGameState: mockSetGameState,
-                    width: mockWidth,
-                    height: mockHeight,
-                }),
-            {
-                wrapper: Wrapper,
-            }
-        );
+    it('корректно инициализирует состояние игры', () => {
+        act(() => {
+            const { result } = renderHook(
+                () =>
+                    useGame({
+                        gameState: GAME_STATE.RUNNING,
+                        onGameOver: mockOnGameOver,
+                        setGameState: mockSetGameState,
+                        width: mockWidth,
+                        height: mockHeight,
+                    }),
+                { wrapper: Wrapper }
+            );
 
-        expect(result.current.snakeBody).toBeDefined();
-        expect(result.current.foodPos).toBeDefined();
+            console.log(result);
+
+            expect(result.current.snakeBody).toBeDefined();
+            expect(result.current.foodPos).toBeDefined();
+        });
     });
 
-    // it('handles snake movement correctly', () => {
-    //     const { result } = renderHook(
-    //         () =>
-    //             useGame({
-    //                 gameState: GAME_STATE.RUNNING,
-    //                 onGameOver: mockOnGameOver,
-    //                 setGameState: mockSetGameState,
-    //                 width: mockWidth,
-    //                 height: mockHeight,
-    //             }),
-    //         { wrapper: Wrapper }
-    //     );
+    it('правильно обрабатывает движение змеи', () => {
+        act(() => {
+            const { result } = renderHook(
+                () =>
+                    useGame({
+                        gameState: GAME_STATE.RUNNING,
+                        onGameOver: mockOnGameOver,
+                        setGameState: mockSetGameState,
+                        width: mockWidth,
+                        height: mockHeight,
+                    }),
+                { wrapper: Wrapper }
+            );
 
-    //     act(() => {
-    //         const mockEvent = {
-    //             code: 'ArrowRight',
-    //         } as unknown as React.KeyboardEvent<Element>;
+            console.log(result);
 
-    //         result.current.onSnakeMove(mockEvent);
-    //     });
-    // });
+            const mockEvent = {
+                code: 'ArrowRight',
+            } as unknown as React.KeyboardEvent<Element>;
+
+            result.current.onSnakeMove(mockEvent);
+        });
+    });
 });
