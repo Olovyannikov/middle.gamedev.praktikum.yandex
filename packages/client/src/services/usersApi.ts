b4bca85ede1f0dce7f43ca9ef11819af57f31204
tinymelon <e.pratusevich@gmail.com>
2023-12-03 15:54:04 +0300

    [PRACTICUM] feat(authorization): integration with Practicum API, redux (GAM-33) (#22)
    * feat(authorization): added integration with Practicum API and PrivateRoute authorization check
    
    * feat(authorization): added registration (GAM-33)
    
    * feat(authorization): profile page connected to Practicum API (except avatar) (GAM-33)
    
    * refactor(authorization): API is correctly divided into modules (GAM-33)
    
    * feat(authorization): change avatar connected to Practicum API (GAM-33)
    
    ---------
    
    Co-authored-by: Ilya Olovyannikov <excaliburgyro@gmail.com>

---
+++ packages/client/src/services/usersApi.ts
@@ -0,0 +1,37 @@
+import { baseApi } from './baseApi';
+import { usersOperations } from '@/shared/constants/api';
+import {
+    avatarRequest,
+    avatarResponse,
+    passwordRequest,
+    passwordResponse,
+} from '@/shared/types/api';
+
+export const usersApi = baseApi.injectEndpoints({
+    endpoints: (builder) => ({
+        changeAvatar: builder.mutation<avatarResponse, avatarRequest>({
+            query: (body) => ({
+                url: usersOperations.avatar,
+                method: 'PUT',
+                body,
+                credentials: 'include',
+            }),
+            invalidatesTags: ['User'],
+        }),
+        changePassword: builder.mutation<passwordResponse, passwordRequest>({
+            query: (body) => ({
+                url: usersOperations.password,
+                method: 'PUT',
+                body,
+                credentials: 'include',
+                responseHandler: (response) => {
+                    if (response.status == 200) return response.text();
+                    return response.json();
+                },
+            }),
+        }),
+    }),
+    overrideExisting: false,
+});
+
+export const { useChangeAvatarMutation, useChangePasswordMutation } = usersApi;
