8ad729fdb86516b60349961fb220a2f8db9a58a6
tinymelon <e.pratusevich@gmail.com>
2024-01-14 21:53:47 +0300

    refactor(forum api): remove unused code (GAM-42)

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -12,20 +12,12 @@ dotenv.config();
 import { configureStore } from '@reduxjs/toolkit';
 import express from 'express';
 import * as fs from 'fs';
-import https from 'https';
 import * as path from 'path';
 
 import { commentModel } from './models/comment';
 import { topicModel } from './models/topic';
 import { userModel } from './models/user';
 import { baseApi } from './store';
-
-type requestOptions = {
-    host: string;
-    path: string;
-    method?: string;
-    headers?: Record<string, string | number>;
-};
 
 const isDev = () => process.env.NODE_ENV === 'development';
 
