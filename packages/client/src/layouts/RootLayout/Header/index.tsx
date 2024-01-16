efb2e3364adfad48798796fd82c2642a066427cf
Evgeniy Talagaev <80750542+WeHaveBOOST@users.noreply.github.com>
2024-01-16 11:37:56 +0300

    feat: add logic to change the color theme (GAM-43) (#38)
    * feat: add logic to change the color theme (GAM-43)
    
    * refactor: add comments (GAM-43)
    
    ---------
    
    Co-authored-by: ee.talagaev <ee.talagaev@tensor.ru>

--- packages/client/src/layouts/RootLayout/Header/index.tsx
+++ packages/client/src/layouts/RootLayout/Header/index.tsx
@@ -1,63 +1,78 @@
 import type { DetailedHTMLProps, HTMLAttributes } from 'react';
-import cn from 'clsx';
-import { AppLink } from '@/shared/ui';
-import { useAuth } from '@/shared/context/AuthContext';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import IconButton from '@mui/material/IconButton';
 import { SignOut, Spinner } from '@phosphor-icons/react';
-import s from './Header.module.scss';
-import { useLogout } from '@/shared/hooks/useLogout';
+import cn from 'clsx';
 
-type HeaderProps = DetailedHTMLProps<
-    HTMLAttributes<HTMLUListElement>,
-    HTMLUListElement
->;
+import { useAuth } from '@/shared/context/AuthContext';
+import { useColorMode } from '@/shared/context/ColorModeProvider';
+import { useIsDarkMode } from '@/shared/hooks/useIsDarkMode';
+import { useLogout } from '@/shared/hooks/useLogout';
+import { AppLink } from '@/shared/ui';
+
+import s from './Header.module.scss';
+
+type HeaderProps = DetailedHTMLProps<HTMLAttributes<HTMLUListElement>, HTMLUListElement>;
 
 export const Header = ({ className, ...props }: HeaderProps) => {
     const { isAuth } = useAuth();
     const { logout, isLoading } = useLogout();
+    const toggleColorMode = useColorMode();
+    const isDarkMode = useIsDarkMode();
 
     return (
         <header>
-            <nav className={s.nav}>
+            <nav className={cn(s.nav, isDarkMode && s.darkNav)}>
                 <ul className={cn(s.list, 'list-reset', className)} {...props}>
                     <li>
-                        <AppLink to="/">Main</AppLink>
+                        <AppLink to='/'>Main</AppLink>
                     </li>
                     {!isAuth && (
                         <>
                             <li>
-                                <AppLink to="/sign-in">Sign-In</AppLink>
+                                <AppLink to='/sign-in'>Sign-In</AppLink>
                             </li>
                             <li>
-                                <AppLink to="/sign-up">Sign-Up</AppLink>
+                                <AppLink to='/sign-up'>Sign-Up</AppLink>
                             </li>
                         </>
                     )}
                     {isAuth && (
                         <li>
-                            <AppLink to="/forum">Forum</AppLink>
+                            <AppLink to='/forum'>Forum</AppLink>
                         </li>
                     )}
                     <li>
-                        <AppLink to="/game">Game</AppLink>
+                        <AppLink to='/game'>Game</AppLink>
                     </li>
 
                     <li>
-                        <AppLink to="/leaderboard">LeaderBoard</AppLink>
+                        <AppLink to='/leaderboard'>LeaderBoard</AppLink>
                     </li>
 
                     <li>
-                        <AppLink to="/me">Profile</AppLink>
+                        <AppLink to='/me'>Profile</AppLink>
                     </li>
+
+                    <li>
+                        <div className={s.colorModeToggle}>
+                            {isDarkMode ? 'Dark' : 'Light'} mode
+                            <IconButton sx={{ ml: 0.5 }} onClick={toggleColorMode} color='inherit'>
+                                {isDarkMode ? <Brightness4Icon /> : <Brightness7Icon />}
+                            </IconButton>
+                        </div>
+                    </li>
+
                     {isAuth && (
                         <li>
-                            <button className={s.logout} onClick={logout}>
+                            <button className={cn(s.logout, isDarkMode && s.darkLogout)} onClick={logout}>
                                 {isLoading ? <Spinner /> : <SignOut />} LogOut
                             </button>
                         </li>
                     )}
                 </ul>
             </nav>
-            <hr className={s.hr} />
         </header>
     );
 };
--- packages/client/src/pages/ForumPage/index.tsx
+++ packages/client/src/pages/ForumPage/index.tsx
@@ -1,34 +1,25 @@
-import { RootLayout } from '@/layouts/RootLayout';
-import { Grid, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { Button, Grid, Typography } from '@mui/material';
+import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+
+import { RootLayout } from '@/layouts/RootLayout';
 
 export default function ForumPage() {
     return (
         <RootLayout>
-            <Grid
-                container
-                alignItems="center"
-                justifyContent="center"
-                direction="column"
-                sx={{ minWidth: '100%', padding: 1 }}
-            >
-                <Typography variant="h4" sx={{ padding: '20px 0' }}>
+            <Grid container alignItems='center' justifyContent='center' direction='column' sx={{ minWidth: '100%' }}>
+                <Typography variant='h4' component='h1' sx={{ padding: '20px 0' }}>
                     Snake's forum page
                 </Typography>
-                <Grid
-                    alignItems="center"
-                    justifyContent="center"
-                    sx={{ width: '90%' }}
-                >
+                <Grid alignItems='center' justifyContent='center' sx={{ width: '90%' }}>
                     <TableContainer component={Paper}>
-                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <Table sx={{ minWidth: 650 }} aria-label='simple table'>
                             <TableHead>
                                 <TableRow>
                                     <TableCell>Название топика</TableCell>
@@ -45,9 +36,7 @@ export default function ForumPage() {
                                     }}
                                 >
                                     <TableCell>
-                                        <Link to="/topic/1">
-                                            Как не умереть за первые 10 секунд
-                                        </Link>
+                                        <Link to='/topic/1'>Как не умереть за первые 10 секунд</Link>
                                     </TableCell>
                                     <TableCell>jxSolo</TableCell>
                                     <TableCell>02.03.2023</TableCell>
@@ -56,12 +45,8 @@ export default function ForumPage() {
                         </Table>
                     </TableContainer>
 
-                    <Grid
-                        container
-                        alignItems="flex-end"
-                        justifyContent="flex-end"
-                    >
-                        <Button component={Link} to="/create/topic">
+                    <Grid container alignItems='flex-end' justifyContent='flex-end'>
+                        <Button component={Link} to='/create/topic'>
                             Create topic
                         </Button>
                     </Grid>
