9c770429d4e0d2d7d4a35c4e0d23b7d5fb6899a5
tinymelon <e.pratusevich@gmail.com>
2024-02-06 23:52:53 +0300

    fix(cloud): api requests, ssr fonts, fullscreen styles (GAM-55)

--- packages/client/src/entities/FullScreen/index.tsx
+++ packages/client/src/entities/FullScreen/index.tsx
@@ -1,13 +1,24 @@
 //TODO fix after `yarn ssr`
-import { Icon } from '@/shared/ui';
-import FullScreenIcon from '@/app/assets/img/fullscreen_icon.svg';
+import { CSSProperties } from 'react';
+
 import FullScreenExitIcon from '@/app/assets/img/fullscreen_exit_icon.svg';
+import FullScreenIcon from '@/app/assets/img/fullscreen_icon.svg';
 import { useFullScreenAPI } from '@/shared/hooks/useFullScreenAPI';
+import { Icon } from '@/shared/ui';
 
 export const FullScreen = () => {
     const { isFullscreen, toggleFullscreen } = useFullScreenAPI();
+    const styles: CSSProperties = {
+        width: '60px',
+        background: 'transparent',
+        border: 'none',
+        position: 'fixed',
+        bottom: '20px',
+        right: '20px',
+        cursor: 'pointer',
+    };
     return (
-        <button id='toggler' onClick={() => toggleFullscreen()}>
+        <button style={styles} id='toggler' onClick={() => toggleFullscreen()}>
             <Icon src={!isFullscreen ? FullScreenIcon : FullScreenExitIcon} />
         </button>
     );
